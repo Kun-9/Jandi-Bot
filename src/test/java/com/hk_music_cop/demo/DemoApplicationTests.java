8c24feab5192ff92a4cb42617c5419f34b346e20
kun <sus5330@icloud.com>
2024-10-17 13:24:20 +0900

    test

---
+++ src/test/java/com/hk_music_cop/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hk_music_cop.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
