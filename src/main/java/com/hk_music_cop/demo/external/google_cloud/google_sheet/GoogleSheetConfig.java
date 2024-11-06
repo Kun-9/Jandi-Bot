ef1514cef165e0541bd5fb81893ddb1c9487ad84
kun <sus5330@icloud.com>
2024-11-07 00:42:29 +0900

    GoogleAPIconfig refactoring

--- src/main/java/com/hk_music_cop/demo/external/google_cloud/google_sheet/GoogleSheetConfig.java
+++ src/main/java/com/hk_music_cop/demo/external/google_cloud/google_sheet/GoogleSheetConfig.java
@@ -1,6 +1,8 @@
 package com.hk_music_cop.demo.external.google_cloud.google_sheet;
 
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.gson.GsonFactory;
 import com.google.api.client.json.jackson2.JacksonFactory;
 import com.google.api.services.sheets.v4.Sheets;
 import com.google.api.services.sheets.v4.SheetsScopes;
@@ -28,30 +30,29 @@ public class GoogleSheetConfig {
 	@Bean
 	public Sheets sheetService(GoogleSheetProperties googleSheetProperties) {
 
+		String applicationName = googleSheetProperties.config().serviceName();
 		String jsonStr = googleSheetProperties.config().key();
-		log.info("Received JSON: {}", jsonStr);
+
+		// JSON 파일 복호화
 		byte[] decode = Base64.getDecoder().decode(jsonStr);
 
-		log.info("decode JSON : {}", new String(decode, StandardCharsets.UTF_8));
+		InputStream credentialsStream = new ByteArrayInputStream(decode);
 
-		InputStream credentialsStream = new ByteArrayInputStream(
-				decode
-		);
-
-		GoogleCredentials credentials;
 		try {
-			credentials = ServiceAccountCredentials.fromStream(credentialsStream)
+			GoogleCredentials credentials = ServiceAccountCredentials.fromStream(credentialsStream)
 					.createScoped(Collections.singleton(SheetsScopes.SPREADSHEETS_READONLY));
 
+			NetHttpTransport httpTransport = GoogleNetHttpTransport.newTrustedTransport();
+			GsonFactory jsonFactory = GsonFactory.getDefaultInstance();
+			HttpCredentialsAdapter credentialsAdapter = new HttpCredentialsAdapter(credentials);
 
-			return new Sheets.Builder(
-					GoogleNetHttpTransport.newTrustedTransport(),
-					JacksonFactory.getDefaultInstance(),
-					new HttpCredentialsAdapter(credentials))
-					.setApplicationName(googleSheetProperties.config().serviceName())
+			return new Sheets.Builder(httpTransport, jsonFactory, credentialsAdapter)
+					.setApplicationName(applicationName)
 					.build();
+
 		} catch (GeneralSecurityException | IOException e) {
 			throw new CustomApiException("API 호출 오류");
 		}
 	}
+
 }
